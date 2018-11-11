9302513ad38659179e207c9d9a3b7da5fe8f4307
whitelightG <femikiki@gmail.com>
2018-11-11 21:22:08 +0000

    Fixed all bugs

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
-// app.set('view cache', true);
+app.set('view cache', true);
 
 app.use(logger('dev'));
 app.use(express.json());
